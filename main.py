3b4e0c3d3bc3b701a9b4f505d3c57d9f05d22e70
Oscar Molinar <molinar.o@latamdigitalmarketing.com>
2022-01-24 16:08:53 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+if "fullname" in input:
+    fullname = input["fullname"]
+    firstname = ""
+    lastname = ""
+    fullname_splitted = fullname.split(" ")
+
+    if len(fullname_splitted) > 1:
+        firstname = fullname_splitted[0]
+        del fullname_splitted[0]
+        lastname = " ".join(fullname_splitted)
+    else:
+        lastname = fullname_splitted[0]
+else:
+    firstname = ""
+    lastname = ""
+
+
+output = [{ "firstname": firstname, "lastname": lastname }]
